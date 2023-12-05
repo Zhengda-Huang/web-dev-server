59520071d0b7d6bfae29909f9b7dde7699f1d536
Eoin Flynn <flynn.j@northeastern.edu>
2023-12-05 00:44:28 -0500

    nother day nother dollar

--- Routes/ReviewRoutes.js
+++ Routes/ReviewRoutes.js
@@ -3,18 +3,33 @@ import * as dao from "../DAO/reviewDao.js";
 function ReviewRoutes(app) {
     const findReviewsByMovie = async (req, res) => {
         const {movieName} = req.params;
-        console.log(movieName)
+        //console.log(movieName)
         const reviews = await dao.findReviewByMovie(movieName);
-        console.log(reviews)
+        //console.log(reviews)
         res.json(reviews);
     };
+
+    const findRecentReviewsByUsername = async (req, res) => {
+        const {username} = req.params;
+        //console.log(username)
+        const reviews = await dao.findRecentReviewByUsername(username);
+        //console.log(reviews)
+        res.json(reviews)
+    }
+
+    const findRecentReviews = async (req, res) => {
+        const reviews = await dao.findRecentReviews();
+        res.json(reviews)
+    }
 
     const createReview = async (req, res) =>{
         const review = await dao.createReview(req.body);
         res.json(review);
     }
 
-    app.get("/api/review/:movieName", findReviewsByMovie);
+    app.get("/api/review/movieName/:movieName", findReviewsByMovie);
+    app.get("/api/review/username/:username", findRecentReviewsByUsername);
+    app.get("/api/review/recent", findRecentReviews)
     app.post("/api/review", createReview)
 
 }
